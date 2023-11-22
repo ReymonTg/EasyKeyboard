e2249eb14fe7245f1e39458a9a6189049c709bc2
َAhJ <AmirHosseinJafari8228@gmail.com>
2023-11-22 22:08:14 +0330

    add Keyboard types

---
+++ src/KeyboardTypes/KeyboardInline.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of Reymon.
+ * Reymon is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Reymon is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Mahdi <mahdi.talaee1379@gmail.com>
+ * @author    AhJ <AmirHosseinJafari8228@gmail.com>
+ * @copyright Copyright (c) 2023, ReymonTg
+ * @license   https://choosealicense.com/licenses/gpl-3.0/ GPLv3
+ */
+
+namespace Reymon\EasyKeyboard\KeyboardTypes;
+
+use Reymon\EasyKeyboard\Keyboard;
+use Reymon\EasyKeyboard\Tools\EasyInline;
+
+final class KeyboardInline extends Keyboard
+{
+    use EasyInline;
+
+    /**
+     * @internal
+     */
+    public function jsonSerialize(): mixed
+    {
+        parent::jsonSerialize();
+        return ['inline_keyboard' => $this->data];
+    }
+}
